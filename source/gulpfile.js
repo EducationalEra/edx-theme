e83f844186b0421c5de27cf0437a5af82d46bb2d
Eugene Moroz <yevhenii@torque-its.com>
2016-11-10 00:22:31 +0100

    get rid of course.css

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -20,17 +20,20 @@ gulp.task('serve', ['sass'], function () {
     browserSync.init({
         proxy: 'http://52.51.3.205/',
         serveStatic: ['app/css', '../lms/static/images'],
-        snippetOptions: {
-            rule: {
+        rewriteRules: [
+            {
                 match: /<link.*lms\-main.*>/i,
-                fn: function (browserSyncJS, match) {
-                    return [
-                      '<link rel="stylesheet" type="text/css" href="/lms-main-v1.css"/>',
-                      browserSyncJS
-                    ].join('');
+                fn: function (req, res, match) {
+                    return '<link rel="stylesheet" type="text/css" href="/lms-main-v1.css"/>';
+                }
+            },
+            {
+                match: /<link.*lms\-course.*>/i,
+                fn: function (req, res, match) {
+                    return '';
                 }
             }
-        }
+        ]
     });
 
 
@@ -56,12 +59,18 @@ gulp.task('sass', function () {
 });
 
 // just copies css to OpenEdX theme specific directory for scss
-gulp.task('copy-css', function() {
+gulp.task('copy-main-css', function() {
     return gulp.src('app/css/lms-main-v1.css')
         .pipe(rename('lms-main-v1.scss'))
         .pipe(gulp.dest(targetPath));
 });
 
-gulp.task('build', ['sass', 'copy-css']);
+gulp.task('copy-course-css', function() {
+    return gulp.src('app/css/lms-course.css')
+        .pipe(rename('lms-course.scss'))
+        .pipe(gulp.dest(targetPath));
+});
+
+gulp.task('build', ['sass', 'copy-main-css', 'copy-course-css']);
 
 gulp.task('default', ['serve']);
