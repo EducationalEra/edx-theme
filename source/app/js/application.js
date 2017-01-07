7ec77ddb617f32060738da1af87f4870add6feaf
Artem Ilchuk <artem.ilchuk@ed-era.com>
2017-01-07 13:41:54 +0200

    fix course-info styles

--- source/app/js/application.js
+++ source/app/js/application.js
@@ -1,3 +1,16 @@
 $(document).on('click', 'button.collapse-navigation', function() {
   $('.wrapper-header .header-navigation').toggleClass('expanded');
 });
+
+/* POP UP */
+$(document).ready(function() {
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
